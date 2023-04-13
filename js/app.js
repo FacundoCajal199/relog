246c0d3b0108b7c4a7043e1b5fa1a9523912e7ed
facundo cajal <facundocajal199@gmail.com>
2023-04-12 23:47:33 -0300

    se actualizo fondo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /*Crear una  web con un reloj que muestre la siguiente información:
 
-dia de la semana (jueves 25) de mes (octubre) del año(2023)*/
+dia de la semana (jueves 25) de  mes (octubre) del año(2023)*/
+function obtenerdatos(){
+    let fecha= document.querySelector("#fecha")
+    let hora= document.querySelector("#hora")
+    let fechaActual = new Date(); // objeto day
+    //console.log(fechaActual);
+    //console.log(fechaActual.getDate());//1-31 dia 11 por ejemplo
+    //console.log(fechaActual.getDay());//0-6 numero del dia de la semana
+    //console.log(fechaActual.getMonth());//0-11 
+    //console.log(fechaActual.getFullYear());
+    
+    const daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    
+    
+    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    
+    
+    fecha.innerHTML=`${daysOfWeek[fechaActual.getDay()]} ${fechaActual.getDate()} de ${months[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
+    
+    //Martes 11 de abril 2023
+    //10:35:00
+    hora.innerHTML= `${fechaActual.getHours()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()}`
+}
+setInterval(obtenerdatos,1000);
